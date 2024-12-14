8295b7d7128928e69db746a5b8a5ea6c048b0a80
Fishybearr <76235767+Fishybearr@users.noreply.github.com>
2024-12-13 19:06:21 -0500

    Add basic blinking script to NPC
    Added a simple Coroutine to change the value of the blink blend shape I created for this model. Still need to add randomization to the time it takes to call a blink so she looks less robotic

--- Assets/NPC/blink.cs
+++ Assets/NPC/blink.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class blink : MonoBehaviour
-{
+public class blink : MonoBehaviour{
+
+    [SerializeField]
+     int blinkAmount = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine("MakeBlink");
+        InvokeRepeating("CallBlinkEnum", .1f, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(1, blinkAmount);
+    }
+
+    void CallBlinkEnum() 
+    {
+        StartCoroutine("MakeBlink");
+    }
+
+    IEnumerator MakeBlink() 
+    {
+        while(blinkAmount < 100) 
+        {
+            yield return new  WaitForSeconds(.001f);
+            blinkAmount+= 10;
+        }
+        while(blinkAmount > 0) 
+        {
+            yield return new WaitForSeconds(.001f);
+            blinkAmount-= 10;
+        }
     }
 }
