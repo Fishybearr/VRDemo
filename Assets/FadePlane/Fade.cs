a925f8cefe7c22912cde3412cee633e86483f331
Fishybearr <76235767+Fishybearr@users.noreply.github.com>
2024-12-14 18:58:37 -0500

    Add some basic logic to NPC look functions
    Added extra rotation checking logic to NPC looking function and started to implement settings panel

---
+++ Assets/FadePlane/Fade.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.Animations;
+using TMPro;
+
+public class Fade : MonoBehaviour
+{
+    Animator animator;
+    public TMP_Dropdown dropDown;
+    public GameObject player;
+
+    [SerializeField]
+    Vector3 largeScale = new Vector3(2,2,2);
+    [SerializeField]
+    Vector3 smallScale = new Vector3(1,1,1);
+    [SerializeField]
+    Vector3 defaultScale = new Vector3(.5f,.5f,.5f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>(); // this should be a requireComponent
+        animator.SetBool("FadeIn", true); //make sure game starts faded out
+    }
+
+
+    //TODO: Refactor all of this
+
+    public void ResizePlayer()
+    {
+        StartCoroutine("FadeInOut");
+        
+    }
+
+    //TODO: try to set up delagating for this. Could be the reason it doesn't fire when teleporting
+    public void TeleportFade()
+    {
+        StartCoroutine("TeleFade");
+        Debug.Log("Faded");
+    }
+
+    public void ChangeFade() 
+    {
+        animator.SetBool("FadeIn",!animator.GetBool("FadeIn"));
+    }
+
+     IEnumerator FadeInOut() 
+    {
+        ChangeFade();
+        yield return new WaitForSeconds(1);
+        if (dropDown.value == 0)
+        {
+            //set defaultScale
+            player.transform.localScale = defaultScale;
+        }
+        else if (dropDown.value == 1)
+        {
+            //set small size
+            player.transform.localScale = smallScale;
+        }
+        else
+        {
+            //set big size
+            player.transform.localScale = largeScale;
+            Debug.Log(dropDown.value.ToString());
+        }
+        ChangeFade();
+    }
+
+    IEnumerator TeleFade() 
+    {
+        ChangeFade();
+        yield return new WaitForSeconds(1);
+        ChangeFade();
+    }
+
+    
+ 
+}
--- Assets/NPC/NPCIK.cs
+++ Assets/NPC/NPCIK.cs
@@ -13,16 +13,57 @@ public class NPCIK : MonoBehaviour
     public GameObject lookTarget;
     Animator animator;
    public bool ikActive = false;
+
+    [SerializeField]
+    float globalWeight = 1.0f;
+    [SerializeField]
+    float bodyWeight = .01f;
+    [SerializeField]
+    float headWeight = .75f;
+
+    GameObject pivot;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        pivot = new GameObject("pivot");
+        pivot.transform.parent = transform;
+        pivot.transform.localPosition = new Vector3(0, 1.5f, 0); //TODO: no magic numbers
+        //this transform will need to be locked somehow so that the animMovement
+        //does not interfere with the looking
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*TODO: if raycast from player is not hitting her
+         *  face then lerp globalWeight to 0 in update()
+         *  
+         *  then add some kind of tracking of this for the smile blendshape
+         *  
+         *  ALSO add support for idle anim transition and movement pause
+         *  this will keep her in place for viewing
+         *  
+         *  also add distance calculation to prevent her from looking at
+         *  player when too far away
+         *  
+         *  ALSO Set x limits on rotation for head
+         */
+
+        pivot.transform.LookAt(lookTarget.transform);
+        float pivotRotY = pivot.transform.localRotation.y;
+       // Debug.Log(pivotRotY);
+
+
+        //TODO: make a duplicate of this system for chest rotation
+        if(pivotRotY < .65f && pivotRotY > -.65f) 
+        {
+            globalWeight = Mathf.Lerp(globalWeight, 1, Time.deltaTime * 2.5f); //TODO: swap 2.5 with a resetFactor float
+        }
+        else 
+        {
+            globalWeight = Mathf.Lerp(globalWeight, 0, Time.deltaTime * 2.5f);
+        }
     }
 
     private void OnAnimatorIK(int layerIndex)
@@ -34,12 +75,13 @@ public class NPCIK : MonoBehaviour
 
                 if (lookTarget != null)
                 {
-                    animator.SetLookAtWeight(1,.01f,.75f); //eyes need to be handled separate as they do not have bones
+                    animator.SetLookAtWeight(globalWeight,bodyWeight,headWeight); //eyes need to be handled separate as they do not have bones
                     animator.SetLookAtPosition(lookTarget.transform.position);
                 }
                 else
                 {
-                    animator.SetLookAtWeight(0,0,0);
+                    //set global weight to 0
+                    animator.SetLookAtWeight(0);
                 }
             }
         }
